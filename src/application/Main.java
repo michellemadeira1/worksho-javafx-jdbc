25f62e91487433c082d3e166087c5a2ec13617c1
Michelle Madeira <michellemadeira1@gmail.com>
2023-01-24 12:43:16 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
@@ -23,7 +25,7 @@ public class Main extends Application {
 	
 	
 	
-	Scene mainScene = new Scene(scrollPane);
+    mainScene = new Scene(scrollPane);
 	primaryStage.setScene(mainScene);
 	primaryStage.setTitle("Sample JavaFX application");
 	primaryStage.show();
@@ -31,6 +33,10 @@ public class Main extends Application {
 	} catch (IOException e) {
 	e.printStackTrace();
 	}
+}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String [] args) {
